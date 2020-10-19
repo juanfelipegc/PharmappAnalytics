f5b1e52868dab7d0d5e7cf165b9e3f16c2795c5f
Sebastián <js.millan10@uniandes.edu.co>
2020-10-18 22:37:52 -0500

    Connecting postgreSQL with Cloud Firestore using cloud functions

--- functions/AnalyticsService/index.js
+++ functions/AnalyticsService/index.js
@@ -1,8 +1,35 @@
 const functions = require('firebase-functions');
+const {dbFirestore} = require('../Firebase');
+const { Client } = require('pg')
 
+// Function to connect with the datanase client
+function conectarBD(){
+    const client = new Client({
+        host: process.env.PGHOST,
+        user: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    })
+    client.connect()
+    return client
+}
 /*
-First function to show the correct deployment of the analytics service
+Function to create a new user in the postgreSQL database once iy is created in the Firebase Cloud Firestore
  */
-exports.analytics = functions.https.onRequest((req, res) => {
-    res.status(200).send("Analytics service working");
+exports.updateUsersData = functions.firestore.document('users/{usersId}').onCreate((snap, context) => {
+    const client = conectarBD()
+    const newUser = snap.data()
+    const text = 'INSERT INTO users(name, age, email, last_name, insurance, gender, city) VALUES($1, $2, $3, $4, $5, $6, $7)'
+    const values = [newUser.name, newUser.age, newUser.email, newUser.lastname, newUser.insurance, newUser.gender, newUser.city]
+    client
+        .query(text, values)
+        .then(res => {
+            console.log(res.rows[0])
+            console.log("New user created")
+            return res.rows[0]
+        })
+        .catch(e => console.error(e.stack))
 });
